034f49ceea1f6a1ef806a9bf786629f2e94dc93f
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2016-09-12 21:01:28 +0100

    Convert from FIB to DEC
    Pairing with Michele Lapi

--- src/clj_fib_to_dec/core.clj
+++ src/clj_fib_to_dec/core.clj
@@ -10,6 +10,33 @@
               (+ (first (fib-seq (dec n))) (second (fib-seq (dec n)))))))) ;; new member to add
 
 
+(defn- fib 
+  ([] (fib 1 1))
+  ([n-1 n-2] (lazy-seq (cons n-2 (fib (+ n-1 n-2) n-1)))))
+
+(defn-
+  char2num
+  [n]
+  (- (int n) (int \0)))
+
+(defn- 
+  chop-in-parts
+  [number]
+  (map char2num number))
+
+(defn-
+  get-fib-values
+  [fib_num]
+  (->> fib_num
+    chop-in-parts
+    reverse
+    (map * (fib))))
+
+;; (fib2dec "1001") -> 4
+(defn
+  fib2dec
+  [fib_num]
+  (reduce + (get-fib-values fib_num)))
 
 
 (defn foo
