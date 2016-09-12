807cb81726601306e2ad2a0e9dae11be8a937c98
petr-tik <petr-tik@users.noreply.github.com>
2016-09-12 20:37:34 +0100

    sort of working lazy fib

--- src/clj_fib_to_dec/core.clj
+++ src/clj_fib_to_dec/core.clj
@@ -1,5 +1,17 @@
 (ns clj-fib-to-dec.core)
 
+(defn fib-seq
+  "Generates a collection that is Fibonacci sequence of length n"
+  [n]
+  (if (= 2 n)
+     '(1 1)
+     (lazy-seq 
+        (conj (fib-seq (dec n)) ;;previous sequence
+              (+ (last (fib-seq (dec n))) (last (butlast (fib-seq (dec n)))))))) ;; new member to add
+)
+
+
+
 (defn foo
   "I don't do a whole lot."
   [x]
