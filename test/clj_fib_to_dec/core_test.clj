6252736a6c57fbc70bd571c1aa852bab928800db
petr-tik <petr-tik@users.noreply.github.com>
2016-09-12 20:03:03 +0100

    added the rest

---
+++ test/clj_fib_to_dec/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-fib-to-dec.core-test
+  (:require [clojure.test :refer :all]
+            [clj-fib-to-dec.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
