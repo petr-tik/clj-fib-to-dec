63994d8bd2724b490f48064c3f53d31854e0a28b
petr-tik <petr-tik@users.noreply.github.com>
2016-09-22 23:19:04 +0100

    added a helper func for and started dec2fib

--- src/clj_fib_to_dec/core.clj
+++ src/clj_fib_to_dec/core.clj
@@ -26,7 +26,7 @@
 (defn-
   char2num
   "Returns the int value of a symbol (\2 \4 etc.)
-  ===> (char2num \2) 
+  ===> (char2num \\2) 
   ===> 2
   "
   [n]
@@ -64,6 +64,47 @@
   "
   [fib_num]
   (reduce + (get-fib-values fib_num)))
+
+
+(defn 
+  fib-digits-from-dec
+  "Given a decimal number and a sequence of relevant (less or equal to dec-num) 
+  fibonacci numbers, returns a vector of digits of fibonacci base
+  ===> (fib-digits-from-dec '(13 8 5 3 2 1 1) 14)
+  ===> \"1000010\"
+  "
+  ([fib-seq dec-num]
+  (fib-digits-from-dec fib-seq dec-num (vector)))
+  ([fib-seq dec-num digit-vec]
+  (cond
+   (empty? fib-seq)
+       (reduce str digit-vec)
+   (or (= 0 dec-num)
+       (< dec-num (first fib-seq)))
+    ;; if decimal number is less than the first of fib-seq 
+    ;; we won't be using that element of the fib-seq. It will be used zero times. 
+       (fib-digits-from-dec (rest fib-seq) dec-num (conj digit-vec 0))
+    ;; otherwise
+   (>= dec-num (first fib-seq))
+   (fib-digits-from-dec (rest fib-seq) (- dec-num (first fib-seq)) (conj digit-vec 1)))))
+
+
+(defn
+  dec2fib
+  "Given a number in decimal form, returns a string with number in fibonacci base
+  ===> (dec2fib 27) 
+  ===> \"10010010\""
+  ;; given a decimal, finds the greatest fib number that is less or equal to the decimal.
+  ;; generate such a fib seq. 
+  ;; eg. 27 -> 
+  ;; fib-seq    (21 13 8 5 3 2 1 1)
+  ;; nums-used  (1  0  0 1 0 0 1 0)
+  ;; then return a sequence of 1s or 0s for each fib number that is used or not
+
+  [dec_num]
+  (dec dec_num)
+  ;;(map bit-or (chop-in-parts "1000") (chop-in-parts "0001") (chop-in-parts "0100")) 
+)
 
 
 (defn foo
