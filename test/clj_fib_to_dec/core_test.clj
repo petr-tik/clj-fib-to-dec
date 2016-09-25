d2f8a03854aaad6e38a23d6df35059588ad6b283
petr-tik <petr-tik@users.noreply.github.com>
2016-09-25 10:36:51 +0100

    added more tests

--- test/clj_fib_to_dec/core_test.clj
+++ test/clj_fib_to_dec/core_test.clj
@@ -7,6 +7,18 @@
   test-char2num
   (is (= (range 10) (map clj-fib-to-dec.core/char2num '(\0 \1 \2 \3 \4 \5 \6 \7 \8 \9)))))
 
+(deftest
+  test-chop-in-parts
+  (is (= '(1 0 0 0 1) (clj-fib-to-dec.core/chop-in-parts "10001")))
+  (is (= '(0 0 0 0 0) (clj-fib-to-dec.core/chop-in-parts "00000")))
+)
+
+(deftest
+  test-fib-seq
+  (is (= '(21 13 8 5 3 2 1 1) (fib-seq 8)))
+  (is (= '(144 89 55 34 21 13 8 5 3 2 1 1) (fib-seq 12)))
+)
+
 
 (deftest 
   test-dec-to-fib-to-dec
